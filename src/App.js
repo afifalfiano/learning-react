6521ad7d1a7862cc0a2f6744b203be20b133c321
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-03-06 07:55:56 +0700

    useState

--- src/App.js
+++ src/App.js
@@ -1,15 +1,12 @@
 import React from 'react';
 import { render } from 'react-dom';
-
-import Pet from './Pet.js';
+import SearchParams from './SearchParams';
 
 const App = () => {
   return (
     <div>
       <h1 id="something-import">Adopt Me</h1>
-      <Pet name="Luna" animal="Dog" breed="Havanese"/>
-      <Pet name="Pepper" animal="Bird" breed="Cockatiel"/>
-      <Pet name="Cat" animal="Doink" breed="Mixed"/>
+      <SearchParams/>
     </div>
   )
 };
