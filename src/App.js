5e4edc7cc165701001c2f907636fa98d16023707
Afif Alfiano <afif.alfiano@uii.ac.id>
2021-03-06 07:35:53 +0700

    change hs to jsx

--- src/App.js
+++ src/App.js
@@ -4,20 +4,14 @@ import { render } from 'react-dom';
 import Pet from './Pet.js';
 
 const App = () => {
-  return React.createElement("div", { id: "something-id", className: "new" }, [
-    React.createElement("h1", {}, "Adopt Me!"),
-    React.createElement(Pet, {
-      name: "Luna",
-      animal: "Dog",
-      breed: "Havanese"
-    }),
-    React.createElement(Pet, {
-      name: "Pepper",
-      animal: "Bird",
-      breed: "Cockatiel"
-    }),
-    React.createElement(Pet, { name: "Doink", animal: "Cat", breed: "Mixed" })
-  ]);
+  return (
+    <div>
+      <h1 id="something-import">Adopt Me</h1>
+      <Pet name="Luna" animal="Dog" breed="Havanese"/>
+      <Pet name="Pepper" animal="Bird" breed="Cockatiel"/>
+      <Pet name="Cat" animal="Doink" breed="Mixed"/>
+    </div>
+  )
 };
 
 render(React.createElement(App), document.getElementById("root"));
